204e17278f69b355e731660b354b9409325c0345
wjstpgh <sehoJ3756@gmail.com>
2024-05-11 18:17:07 +0900

    first r3f application

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience";
+import { ACESFilmicToneMapping, SRGBColorSpace } from "three";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <Canvas
+      flat
+      dpr={[1, 2]} //defualt value
+      gl={{
+        antialias: true,
+        toneMapping: ACESFilmicToneMapping,
+        outputColorSpace: SRGBColorSpace,
+      }}
+      camera={{ fov: 85, near: 0.1, far: 200, position: [3, 2, 6] }}
+    >
+      <Experience />
+    </Canvas>
+  );
 }
