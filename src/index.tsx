da171d71d1332544085137ff06be64bb4b3c209e
wjstpgh <sehoJ3756@gmail.com>
2024-05-18 16:55:29 +0900

    디버깅 툴 leva, 모니터링 툴 perf 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
+import { StrictMode } from "react";
 import App from "./App";
 import "./style.css";
 import { createRoot } from "react-dom/client";
+import { Leva } from "leva";
 
 const root = createRoot(document.querySelector("#root") as Element);
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <Leva collapsed />
+    <App />
+  </StrictMode>
+);
