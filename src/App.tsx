8452dadba555b79f2fa7984ea57fbaddea7b97a2
wjstpgh <sehoJ3756@gmail.com>
2024-06-02 19:39:56 +0900

    백그라운드와 환경맵 세팅 종류

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Experience from "./Experience";
 import { ACESFilmicToneMapping, SRGBColorSpace } from "three";
 
 export default function App() {
+  // const created = ({ scene }: RootState) => {
+  //   scene.background = new Color("red");
+  // };
+
   return (
     <Canvas
       flat
@@ -13,6 +17,8 @@ export default function App() {
         outputColorSpace: SRGBColorSpace,
       }}
       camera={{ fov: 85, near: 0.1, far: 200, position: [3, 2, 6] }}
+      shadows={false}
+      // onCreated={created}
     >
       <Experience />
     </Canvas>
--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,18 +1,26 @@
 import {
+  ContactShadows,
+  Environment,
   Float,
   Html,
   MeshReflectorMaterial,
   OrbitControls,
   PivotControls,
+  Sky,
   Text,
   TransformControls,
+  useHelper,
 } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
 import { button, useControls } from "leva";
 import { Perf } from "r3f-perf";
-import { useRef } from "react";
-import { Mesh } from "three";
+import { useEffect, useRef } from "react";
+import { DirectionalLight, DirectionalLightHelper, Mesh } from "three";
 
 export default function Experience() {
+  const directionalLight = useRef<DirectionalLight>(null!);
+  useHelper(directionalLight, DirectionalLightHelper, 1);
+
   const sphere = useRef<Mesh>(null!);
   const cube = useRef<Mesh>(null!);
 
@@ -29,15 +37,115 @@ export default function Experience() {
     choice: { options: ["a", "b", "c"] },
   });
 
+  const { shadowColor, opacity, blur } = useControls("contact shadows", {
+    shadowColor: "#000000",
+    opacity: { value: 0.5, min: 0, max: 1 },
+    blur: { value: 1, min: 0, max: 10 },
+  });
+
+  const { sunPosition } = useControls("sunPosition", {
+    sunPosition: [1, 2, 3],
+  });
+
+  const { envMapIntensity } = useControls("environment map", {
+    envMapIntensity: { value: 1, min: 0, max: 12 },
+  });
+
+  const scene = useThree((state) => state.scene);
+  useEffect(() => {
+    scene.environmentIntensity = envMapIntensity;
+  }, [envMapIntensity]);
+
   const { perfVisible } = useControls({ perfVisible: false });
+
+  useFrame((_state, delta) => {
+    // const time = state.clock.elapsedTime;
+    // cube.current.position.x = 2 + Math.sin(time);
+    cube.current.rotation.y += delta * 0.2;
+  });
 
   return (
     <>
+      {/* <BakeShadows />
+      <SoftShadows size={25} samples={10} focus={0} /> */}
+      <color args={["ivory"]} attach="background" />
       {perfVisible && <Perf position="top-left" />}
 
+      <Environment
+        // background
+        // ground={{ height: 7, radius: 28, scale: 100 }}
+        // files={"./environmentMaps/the_sky_is_on_fire_2k.hdr"}
+        preset="apartment"
+        // files={[
+        //   "./environmentMaps/2/px.jpg",
+        //   "./environmentMaps/2/nx.jpg",
+        //   "./environmentMaps/2/py.jpg",
+        //   "./environmentMaps/2/ny.jpg",
+        //   "./environmentMaps/2/pz.jpg",
+        //   "./environmentMaps/2/nz.jpg",
+        // ]}
+        // resolution={32}
+      >
+        {/* <color args={["black"]} attach="background" />
+        <Lightformer
+          position-z={-5}
+          scale={10}
+          color="red"
+          intensity={10}
+          form="ring"
+        /> */}
+        {/* <mesh position-z={-5} scale={10}>
+          <planeGeometry />
+          <meshBasicMaterial color={[1, 0, 0]} />
+        </mesh> */}
+      </Environment>
+
       <OrbitControls makeDefault />
-      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+
+      {/* <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        color="#316d39"
+        opacity={0.8}
+        frames={Infinity}
+        temporal
+        blend={100}
+      >
+        <RandomizedLight
+          position={[1, 2, 3]}
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={3}
+          bias={0.001}
+        />
+      </AccumulativeShadows> */}
+
+      <ContactShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        resolution={512}
+        far={5}
+        color={shadowColor}
+        opacity={opacity}
+        blur={blur}
+        frames={1}
+      />
+
+      <directionalLight
+        ref={directionalLight}
+        position={sunPosition}
+        intensity={4.5}
+        castShadow
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-top={2}
+        shadow-camera-right={2}
+        shadow-camera-bottom={-2}
+        shadow-camera-left={-2}
+      />
       <ambientLight intensity={1.5} />
+
+      <Sky sunPosition={sunPosition} />
 
       <PivotControls
         anchor={[0, 0, 0]}
@@ -51,6 +159,7 @@ export default function Experience() {
           ref={sphere}
           position={[position.x, position.y, 0]}
           visible={visible}
+          castShadow
         >
           <sphereGeometry />
           <meshStandardMaterial color={color} />
@@ -72,7 +181,12 @@ export default function Experience() {
       </mesh>
       <TransformControls object={cube} mode="translate" />
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh
+        // receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
         <planeGeometry />
         <MeshReflectorMaterial
           mirror={0.5}
@@ -82,6 +196,28 @@ export default function Experience() {
           color="greenyellow"
         />
       </mesh>
+
+      {/* <Stage
+        shadows={{ type: "contact", opacity: 0.2, blur: 3 }}
+        environment="dawn"
+        preset="portrait"
+        intensity={2}
+      >
+        <mesh
+          ref={sphere}
+          position={[position.x, position.y, 0]}
+          visible={visible}
+          castShadow
+        >
+          <sphereGeometry />
+          <meshStandardMaterial color={color} />
+        </mesh>
+        <mesh ref={cube} position-x={2} scale={1.5}>
+          <boxGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+        <TransformControls object={cube} mode="translate" />
+      </Stage> */}
 
       <Float speed={5} floatIntensity={2}>
         <Text
